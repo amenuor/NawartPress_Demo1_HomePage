679190e5c68123392a4c63ece78965020146f3b2
Angelo <angelonicolosi@yanchware.com>
2015-03-29 14:15:23 +0200

    Continue development of requirements
    Menu with images
    moving trains and so on

--- js/timeline.js
+++ js/timeline.js
@@ -1,6 +1,13 @@
 $(document).ready(function($){
 	var $timeline_block = $('.cd-timeline-block');
-
+	$('#train').hide();
+	
+	$('.over').each(function(index){
+		var elem = $(this);
+		elem.hover(function(){$(this).fadeOut()});
+		elem.parent().find('img').mouseout(function(){elem.fadeIn()});
+	});
+	
 	//hide timeline blocks which are outside the viewport
 	$timeline_block.each(function(){
 		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
@@ -13,6 +20,8 @@ $(document).ready(function($){
 		$timeline_block.each(function(){
 			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
 				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+				$('#train').show();
+				$('#train').animate({top: $(this).position().top }, 2000);
 			}
 		});
 	});
